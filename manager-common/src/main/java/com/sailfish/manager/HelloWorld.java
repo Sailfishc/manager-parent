47a093f3aab92f2f97904d25301f32e71c0b846e
sailfishc <mr.cheng94@foxmail.com>
2016-12-04 21:19:29 +0800

    新建了分支，添加了HelloWorld代码

---
+++ manager-common/src/main/java/com/sailfish/manager/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.sailfish.manager;
+
+/**
+ * Created by travis on 2016/12/4.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
